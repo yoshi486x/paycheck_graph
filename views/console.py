729c0a5f4441dfd82195f22eb7e714c554afc274
dev-yoshiki <dev-yoshiki@c4ke.local>
2020-02-04 09:48:27 +0900

    Modified pdf_reader.py and added console.py from viewsfolder

---
+++ views/console.py
@@ -0,0 +1,15 @@
+"""Utils to display to be returned to the user on the console."""
+import os
+import pathlib
+import string
+import sys
+
+def get_base_dir_path():
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
