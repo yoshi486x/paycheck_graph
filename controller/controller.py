c5e8c461544490f24671228a7cffc6ed8c12d5bc
dev-yoshiki <dev-yoshiki@c4ke.local>
2020-02-08 12:13:25 +0900

    Added conversation if mongodb is needed or not.

--- controller/controller.py
+++ controller/controller.py
@@ -5,7 +5,7 @@ from models import full_analyser
 
 def paycheck_analyser():
     """Handles every process of paycheck-graph package."""
-    fullAnalyser = full_analyser.FullAnalyzer()
+    fullAnalyser = full_analyser.FullAnalyser()
     fullAnalyser.convert_pdf_into_text()
-    fullAnalyser.format_text_data_to_analyzable_dict()
+    fullAnalyser.format_text_data_to_analysable_dict()
     fullAnalyser.paycheck_analysis()
