4c93eb1381e8ec6691a5a61e6db3aefe5ae60532
dev-yoshiki <ben.nakagawa01@gmail.com>
2020-02-29 15:17:34 +0900

    Modified controller

--- controller/controller.py
+++ controller/controller.py
@@ -5,8 +5,10 @@ from models import paycheck_analyzer
 
 def paycheck_analysis():
     """Handles every process of paycheck-graph package."""
-    full_analyzer = paycheck_analyzer.AnalyzerModel()
+    analyzer = paycheck_analyzer.AnalyzerModel()
+    full_analyzer = paycheck_analyzer.FullAnalyzer()
+
     full_analyzer.ask_for_db_activation()
-    full_analyzer.convert_pdf_into_text()
-    full_analyzer.format_text_data_to_analysable_dict()
+    analyzer.convert_pdf_into_text()
+    analyzer.format_text_data_to_analysable_dict()
     full_analyzer.visualize_income_timechart()
